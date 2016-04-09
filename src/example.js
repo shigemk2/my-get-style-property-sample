4e1c13e22242239314eed6e4ccdd090cae334fa1
shigemk2 <i.am.shige@gmail.com>
2016-04-09 16:52:04 +0900

    Add sample

--- src/example.js
+++ src/example.js
@@ -1,1 +1,8 @@
-console.log("hogehoge");
+// 該当する要素がなかったら死ぬ
+var foo              =  document.querySelector('.foo');
+var bar              =  document.querySelector('.bar');
+var getStyleProperty =  require('get-style-property');
+
+console.log(getStyleProperty(foo, 'min-height')); // => 20px
+console.log(getStyleProperty(foo, 'max-height')); // none
+console.log(getStyleProperty(bar, 'max-height')); // => 900px
